8fff8203b215cf09a0d1c927a67896666e6d8661
lais <146146262+laissilva04@users.noreply.github.com>
2024-03-10 23:43:32 -0300

    Create Main.java

---
+++ Conta/Main.java
@@ -0,0 +1,8 @@
+package Pessoa;
+
+public class Main {
+    public static void main(String[] args){
+        Conta lais = new Conta(62265988745L, 85649856, 703.48, false);
+        lais.mostrar();
+    }
+}
